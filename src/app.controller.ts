68227015f9f0f1d23999dbe3b6c432035da2515e
Karthick Ragavendran <karthickthankyou@gmail.com>
2023-07-24 13:39:42 +0530

    update the home screen for api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,31 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello()
+    return `
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <title>IKEA Clone API</title>
+        <style>
+          body {
+            background-color: #f5f5f5;
+            text-align: center;
+            padding: 50px;
+            font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+          }
+          a {
+            color: #333;
+            text-decoration: none;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Welcome to the IKEA Clone API</h1>
+        <p>This is a GraphQL API for the IKEA Clone.</p>
+        <p>Click <a href="/graphql">here</a> to access the GraphQL playground.</p>
+        <p>Click <a href="https://ikea.iamkarthick.com">here</a> to see the beautiful project. ;)</p>
+      </body>
+      </html>
+    `
   }
 }
